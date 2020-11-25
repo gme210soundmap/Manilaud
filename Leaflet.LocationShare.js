59f9f8fb035fe3ad5b04b3b28f232a41c9a902ff
gme210soundmap <74966287+gme210soundmap@users.noreply.github.com>
2020-11-25 15:28:08 +0800

    Update Leaflet.LocationShare.js

--- Leaflet.LocationShare.js
+++ Leaflet.LocationShare.js
@@ -1,3 +1,4 @@
+
 L.LocShare = {}
 var LS = L.LocShare
 LS.Send = {}
@@ -40,7 +41,7 @@ L.Control.ShareLocation = L.Control.extend({
         this.link = L.DomUtil.create('a', 'leaflet-bar-part', container);
         this.link.title = this.options.title;
         var userIcon = L.DomUtil.create('img' , 'img-responsive' , this.link);
-        userIcon.src = 'https://github.com/gme210soundmap/Manilaud/blob/main/assets/locatorshare.png'
+        userIcon.src = 'https://raw.githubusercontent.com/CliffCloud/Leaflet.LocationShare/master/dist/images/IconLocShare.png'
         this.link.href = '#';
 
         L.DomEvent.on(this.link, 'click', this._click, this);
